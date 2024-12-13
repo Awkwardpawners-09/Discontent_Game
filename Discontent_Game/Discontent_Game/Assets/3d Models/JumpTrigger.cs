ca9446a02fc5f4f22cce3aa22094c9358c67e0ef
H3xaaa <148347298+H3xaaa@users.noreply.github.com>
2024-12-14 00:38:06 +0800

    Update JumpTrigger.cs

--- Discontent_Game/Discontent_Game/Assets/3d Models/JumpTrigger.cs
+++ Discontent_Game/Discontent_Game/Assets/3d Models/JumpTrigger.cs
@@ -1,31 +1,26 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class JumpTrigger : MonoBehaviour
 {
-    public AudioSource Scream;           // The scream sound
-    public GameObject ThePlayer;         // The player object
-    public GameObject JumpCam;           // The jumpscare camera
-    public GameObject FlashImg;          // The flash effect
-
-    void OnTriggerEnter(Collider other)
+    public AudioSource Scream;
+    public GameObject ThePlayer;
+    public GameObject JumpCam;
+    public GameObject FlashImg;
+    void OnTriggerEnter()
     {
-        if (other.gameObject == ThePlayer)
-        {
-            Scream.Play();
-            JumpCam.SetActive(true);
-            ThePlayer.SetActive(false);
-            FlashImg.SetActive(true);
-            StartCoroutine(EndJump());
-        }
+        Scream.Play();
+        JumpCam.SetActive(true);
+        ThePlayer.SetActive(false);
+        FlashImg.SetActive(true);
+        StartCoroutine(EndJump());
     }
-
     IEnumerator EndJump()
     {
-        // Wait for the jumpscare duration
         yield return new WaitForSeconds(2.03f);
-
-        // Notify GameManager to handle respawn or restart
-        GameManager.Instance.HandleRespawn();
+        ThePlayer.SetActive(true);
+        JumpCam.SetActive(false);
+        FlashImg.SetActive(false);
     }
 }
